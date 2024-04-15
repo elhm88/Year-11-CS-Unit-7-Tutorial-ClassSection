7ddba06a450db333794e23e808e159c11e189594
elhm88 <elhm88@student.cis.edu.hk>
2024-04-15 12:44:58 +0800

    Passed all tests except for Test 5.

--- src/ClassSection.java
+++ src/ClassSection.java
@@ -1,5 +1,89 @@
+import java.util.*;
 
 public class ClassSection {
+    // Properties
+    private String subject;
+    private int capacity;
+    private int yearLevel;
+    private ArrayList<Student> students;
 
+    // Constructors
+    public ClassSection(String subject, int capacity, int yearLevel) {
+        this.subject = subject;
+        this.capacity = capacity;
+        this.yearLevel = yearLevel;
+        students = new ArrayList();
+    }
 
+    // Methods
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getYearLevel() {
+        return yearLevel;
+    }
+
+    public void setYearLevel(int yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student a) {
+        boolean same = false;
+
+        if (a.getYearLevel() != yearLevel) {
+            same = true;
+        }
+
+        for (Student student : students) {
+            if (student.equals(a)) {
+                same = true;
+            }
+        }
+
+        if (same == false) {
+            students.add(a);
+        }
+
+    }
+
+    public void removeStudent(Student a) {
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).equals(a)) {
+                students.remove(i);
+            }
+        }
+    }
+
+    public boolean isStudentEnrolled(Student a) {
+        boolean same = false;
+
+        for (Student student : students) {
+            if (student.equals(a)) {
+                same = true;
+            }
+        }
+
+        return same;
+    }
+
+    public String toString() {
+        return "ClassSection{subject='" + subject + "', capacity=" + capacity + ", yearLevel=" + yearLevel + ", students=" + getStudents() + "}";
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,79 @@
-public class Student {
+import javax.swing.text.html.parser.TagElement;
 
+public class Student {
+    // Properties
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int yearLevel;
+    private static int studentCount;
+
+    // Constructors
+    public Student(String firstName, String lastName, int age, int yearLevel) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearLevel = yearLevel;
+        id = studentCount;
+        studentCount++;
+    }
+
+    // Methods
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getYearLevel() {
+        return yearLevel;
+    }
+
+    public void setYearLevel(int yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+
+    public int getStudentCount() {
+        return studentCount;
+    }
+
+    public boolean equals(Student student) {
+        boolean same = false;
+
+        if (student.getFirstName().equals(firstName) && student.getLastName().equals(lastName) && student.getAge() == age && student.yearLevel == yearLevel) {
+            same = true;
+        }
+
+        return same;
+    }
+
+    public String toString() {
+        return "Student{id=" + id + ", firstName='" + firstName + "', lastName='" +lastName + "', age=" + age + ", yearLevel=" + yearLevel + "}";
+    }
+
+    // "Student{id=77, firstName='Adrian', lastName='Wong', age=17, yearLevel=12}"
 
 }
